14534a138f0342a9e24fd9b50cf67b0ec7b641e3
haoii15 <hli078@uit.no>
2021-11-22 10:58:32 +0100

    ferdig

--- opg4.c
+++ opg4.c
@@ -5,23 +5,84 @@ struct kontrollelement {
     double strekktid;
 };
 
-
+// Oppgave A
 int kontroll(list_t *spor, list_t *fasit)
 {
+    // Deklarerer iteratorer for hver liste og kontrollelement-pekere for hver liste
     listiter_t* sporiter = list_createiter(spor);
     listiter_t* fasititer = list_createiter(fasit);
     kontrollelement_t* spor, *fasit;
+
+    // henter ut første item fra fasit
     fasit = list_next(fasititer);
 
+    // Loopen itererer gjennom fasit 
     while (fasit != NULL)
     {
+        // Henter et og et item fra spor lista
         spor = list_next(sporiter);
+
+        // Dersom spor er null, har ikke deltakeren hvert innom alle postene
         if (spor == NULL)
             return 0;
 
+        // Dersom kodene ikke er like, har deltakeren ikke hvert innom i korrekt rekkefølge
         if (fasit->kode != spor->kode)
             return 0;
     }
 
     return 1;
+}
+
+// Oppgave B
+double tid(list_t *spor)
+{
+    // Deklarer iterator og kontrollelement-peker
+    listiter_t* iter = list_createiter(spor);
+    kontrollelement_t* elem = list_next(iter);
+
+    // Lager en variabel til å lagre totaltiden
+    double totaltid = 0;
+
+    // Itererer lista
+    while (elem != NULL)
+    {
+        // Legger til hver strekktid på totaltid
+        totaltid += elem->strekktid;
+
+        // henter ut neste item
+        elem = list_next(iter);
+    }
+
+    // Returnerer den totale tiden
+    return totaltid;
+}
+
+double bestetid(list_t *godkjente)
+{
+    // Deklarerer variabler som brukes i funksjonen
+    double best = 0;
+    double current;
+
+    // Lager iterator og listepeker
+    listiter_t* iter = list_createiter(godkjente);
+    list_t* deltaker = list_next(iter);
+
+    // Itererer den ytterste lista
+    while (deltaker != NULL)
+    {
+        // Bruker tid funksjonen til å regne ut den totale tida for hver deltaker
+        current = tid(deltaker);
+
+        // Setter best til å være current tiden når best er 0, dvs første iterasjon
+        if (best = 0)
+            best = current;
+
+        // Deretter endres kun best hvis current er mindre enn best
+        else if (current < best)
+            best = current;
+    }
+
+    // Returnerer best, som er den laveste totaltiden
+    return best;
 }
--- oppgave3.c
+++ oppgave3.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+Hjelpefunksjon brukt til å lage et array til å teste funksjonene
+ikke relevant til selve eksamensoppgaven
+*/
 void random_array(int *A, int n)
 {
     for (int i = 0; i < n; i++)
@@ -9,58 +13,79 @@ void random_array(int *A, int n)
     }
 }
 
+// oppgave A
 int forekommer(int *A, int n, int k)
 {
+    // Lager en counter integer til å telle antall ganger k forekommer
     int count = 0;
 
+    // Itererer arrayet ved en for loop
     for (int i = 0; i < n; i++)
     {
+        // Hver gang vi finner k, legger vi til 1 på counteren
         if (A[i] == k)
         {
             count++;
         }
     }
 
+    // Når programmet har kjørt gjennom hele arrayet, returnerer vi antall ganger k forekommer
     return count;
 }
 
+// oppgave B
 void sorter(int *A, int n)
 {
+    // Deklarerer to variabler som brukes i sorteringen
     int temp;
     int sorted = 0;
 
+    // Ønsker å kjøre sorteringsloopen så lenge arrayet ikke er sortert
     while (!sorted)
-    {
+    {   
+        // Starter hver iterasjon med å anta et arrayet er sortert
         sorted = 1;
 
-        for (int i = 0; i < n; i++)
+        // For loop som itererer arrayet
+        for (int i = 0; i < n - 1; i++)
         {
+            // Hvis er element er større enn det som kommer etter, må man bytte plass på elementene
             if (A[i] > A[i + 1])
             {
+                // Swapper to elementer ved å bruke en temp variabel
                 temp = A[i];
                 A[i] = A[i + 1];
                 A[i + 1] = temp;
+
+                // Nå som vi har måttet swappe to elementer, kan vi ikke lenger anta at lista er sortert, derav settes den til 0
                 sorted = 0;
             }
         }
     }
 }
 
+// Oppgave C
 int majoritetselement(int *A, int n)
 {
+    // Bruker sorteringsfunksjonen til å sortere arrayet
     sorter(A, n);
 
+    // Finner midterste tallet
     int midten = A[n/2];
 
+    // Sjekker hvor mange ganger midterste tallet forekommer ved forekommerfunksjonen
     int antall_midten = forekommer(A, n, midten);
 
+    // Dersom midterste tallet er et majoritetselement, returnerer vi 1
     if (antall_midten > n/2)
         return 1;
     
+    // Hvis ikke, 0
     else 
         return 0;
 }
 
+// Main brukt til å teste funksjonene
 int main()
 {
     int n = 1000;
